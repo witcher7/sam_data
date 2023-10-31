8097b0e6a62359163c4fea1403623b866385025c
Rishabh Gulati <gulati.r7121@gmail.com>
2023-10-31 08:25:25 +0530

    Create party.py

---
+++ party.py
@@ -0,0 +1,201 @@
+# # print("WORLD IS gOING CRAZY")
+# #       # "HELLO WORLD"
+
+
+# # # DO THE TASK
+
+# # # PRINT YOUR 5 FAVOURITE FOOD and send me in the chat
+# # print("Mango")
+# # print("Pineapple")
+# # print("Apple")
+# # print("Grapes")
+
+
+# # 10s translate 
+# # we are writting python
+# # interpreter will translate line by line
+# # computer will understand --> process --> do it
+
+# # print is a command which we are giving to computer
+
+
+
+
+# # main.py ? --> .py is an extension
+# # .mp3 --> audio
+# # .mp4 --> videos
+# # .py --> python file
+# # .js --> javascript file
+# # .css --> css file
+# # .html --> html file
+# # .txt --> text file
+# # .md --> markdown file
+
+# # DATA TYPE
+# # TYPES OF DATA --> 
+# # INTEGER --> 1, 2, 3, 4, 5
+# # numbers from -ve to +ve
+# # includes numbers which are negative as well as Positive
+# # examples --> 1,2,3,4, -5,-15,255 etc
+# print(type(100))
+# print(-500)
+# # FLOAT 
+# # floats are numbers with decimals
+# # FLOAT --> 1.23, 3.14, 10.0, 100.0
+# print(type(100.001))
+
+# # type --> shows type of data
+
+
+# # STRING --> "hello", "world", "123", "123.456", "a", "b", "c"
+# # storing word --> alphabet symbol or character
+# print("Hello Coursdeck")
+# # whenver you want to write in strings --> you have to use " "
+# # "  " double quotes
+
+# print("$500")
+# print(type("@Wakaoo123"))
+
+# # BOOLEAN --> it will  store either TRUE or False
+
+# print(True)
+# print(type(False))
+
+# # use for conditions
+
+# # STORE DATA --> store data --> storage buckets # in which 
+# # store data
+
+# # VARIABLE --> container to store data
+# yash = "Mango"
+# print(yash)
+
+# num1 = 200
+# num2 = 300
+# print(num1)
+# print(num2)
+
+# OPERATIONS --
+# 1. ARITHMETIC OPERATIONS
+# 1. ADDITION 
+# num1 = 200
+# num2 = 300
+# print(num1+num2)
+
+# 2. SUBTRACTION 
+
+# num1 = 200
+# num2 = 300
+# print(num1-num2)
+
+# 3. MULTIPLICATION
+# num1 = 200
+# num2 = 300
+# print(num1*num2)
+
+# 4. DIVISION
+# num1 = 200
+# num2 = 300
+# print(num1/num2)
+
+# REMAINDER
+
+# num1 = 200
+# num2 = 300
+# print(num1%num2)
+
+
+# # POWER
+# print(2**4)
+
+
+# #we are calling the variables 
+
+
+# # # Lets
+
+
+
+
+# # STRING --> "hello", "world", "123", "123.456", "a", "b", "c"
+
+# # BOOLEAN --> True, False
+# # LIST --> ["hello", "world", "123", "123.456", "a", "b", "c"]
+# # DICTIONARY --> {"hello": "world", "123": "123.456", "a": "b", "c
+
+# num1 = int(input("Enter number for num1: "))
+# num2 = int(input("Enter number for num2: "))
+# print(num1+num2)
+# but here we are saving the data inside the variable
+# I want that anyone can give the input -->
+# input (take input from the user)
+# DO IT FROM SUBTRACTION AND MULTIPLICATION --> 
+# send me in the chat
+# coding se allergy --> devops  
+
+# DEVELOPER + OPERATIONS ==> IN CLOUD --> you dont need
+# DEVOPS --> but you handle programs and code
+
+# num1 = 100
+# num2 = 25
+# print(num1+num2)
+# print(num1 -num2)
+# print(num1 * num2)
+# print(num1 / num2)
+# print(num1 % num2)
+
+# TYPECASTING 
+# n is storing 21 --> integer/ number
+# n = "WORD"
+# print(type(float(n)))
+
+# write the type of data you want in front of them
+
+# I CAN NOT MAKE STRING --> INTEGER forcefully
+
+
+# IF CONDITION --> we can logic in our programs
+
+# n  = 200
+# if n <200:
+#   print("n is less than 200")
+#   # if condition --> true print statement nahi 
+# else:
+#   print("n is greater than 200")
+
+
+# # CONDITIONAL OPERATORS
+# < --> less than
+# > -->  greater than
+# >= --> greater than or equal to 
+# <= --> less than or equal to 
+# == --> equal to 
+# != --> not equal to
+
+# double equal here means --> we are checking that 
+# whether n has 200 or not
+n  = 200
+
+
+# if n ==200:
+#   print("n is equal to 200")
+#   # if condition --> true print statement nahi 
+# else:
+#   print("n is storing some other value")
+
+#  = --> we are storing somethin
+# == --> we are checking whether that variable is storing any vaule or not
+
+# If I want to add more conditions
+n = int(input("Enter your num: "))
+if n ==20:
+  print("n is 20")
+elif n==10:
+  print("ni is 10")
+elif n==100:
+  print("n is 100")
+else:
+  print("n is a different value")
+
+# CALCULATOR PROJECT
+# STONE PAPER SCISSORS GAME
